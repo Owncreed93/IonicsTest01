2a4f4eac066296616a7ac130f8f90b5022f87813
owncreed93 <christiantarifer@gmail.com>
2020-06-11 18:25:27 -0500

    Agregando la lista de pendientes

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from 'src/app/models/lista.model';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -9,6 +11,48 @@ import { Lista } from 'src/app/models/lista.model';
 })
 export class Tab1Page {
 
-  constructor( public deseosService: DeseosService ) {  }
+  constructor( public deseosService: DeseosService,
+               private router: Router,
+               public alertCtl: AlertController ) {  }
 
+  async agregarLista() {
+    // this.router.navigateByUrl('/tabs/tab1/agregar');
+
+    const alert = await this.alertCtl.create({
+      cssClass: 'my-custom-class',
+      header: 'Nueva Lista',
+      inputs: [
+        {
+          name : 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: (data) => {
+            console.log(data);
+
+            if ( data.titulo.length === 0 ){
+              return;
+            }
+            if ( data.titulo.length >= 0 ){
+              this.deseosService.crearLista( data.titulo );
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+  }
 }
